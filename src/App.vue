a85e531480af7371d93db614b54dcb42002b910b
Sorangel <mejiasory96@gmail.com>
2020-05-29 18:24:21 -0400

    primer commit desafio dfinal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+<div>
+  <h1>Agrega tareas a tu lista</h1>
+  <label class="la">Tarea</label>
+<input type="text" v-model="tarea"/>
+<button @click="add">Agregar Tarea</button>
+</div>
+<compHijo :tareas="tareas" @eliminarTarea="eliminar"></compHijo>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import compHijo from "./components/compHijo.vue";
 
 export default {
-  name: 'App',
+name: 'app',
   components: {
-    HelloWorld
-  }
+    compHijo
+    },
+  data() {
+return {
+  tarea:" ",
+tareas: [],
+
+};
+},
+methods: {
+add() {
+  if(this.tarea !== "") { 
+this.tareas.push(this.tarea),
+this.tarea ="";
+}else{
+  alert("Atención: Se debe ingresar una tarea")
 }
+},
+eliminar(index){
+  this.tareas.splice(index,1);
+}
+}
+};
+
 </script>
 
-<style lang="scss">
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Calibri', sans-serif;
+  color: rgb(11, 153, 23);
+  margin: 15px;
+  
+ 
+}
+.la{ 
+  color: black;
+  font-weight: bold;
+  font-size: 20px;
+  margin-right: 5px;
 }
 </style>
---
+++ src/components/compHijo.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+<ul class="la"> 
+  <li v-for="(tarea,index) in tareas" :key="index">{{tarea}}<button @click="eliminar(index)">Eliminar</button> </li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+name: "compHijo",
+props: {
+dato: String,
+tareas: Array
+},
+methods:{
+  eliminar(index){
+    this.$emit("eliminarTarea", index);
+  }
+}
+};
+</script>
+
+
+<style scoped>
+.la{ 
+  color: black;
+
+}
+
+</style>
